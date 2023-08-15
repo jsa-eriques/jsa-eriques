bb0d89eac0c706a4bc16677771467094b0237627
Ustr4 <jsa.eric@hotmail.com>
2023-08-15 19:46:20 -0300

    more edit

--- src/pages/Hero.js
+++ src/pages/Hero.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Hero.css';
 import { Link } from 'react-scroll';
 import Imu from '../assets/[removal.ai]jsa-eriques-1.png';
+import Work from '../assets/clipart2655834.png'
 import { useTypewriter } from 'react-simple-typewriter';
 
 function Hero () {
@@ -24,15 +25,16 @@ function Hero () {
         <br />
         <div className="hero-btns">
           <button className="hire-me">
-            <a href="https://drive.google.com/file/d/1hms42PdGqHVmWJ1pqyUhPXhwbFqzhsRf/view?usp=drive_link" target="_blank" rel="noreferrer">
-              MEU CV
-            </a>
+              <a href="https://drive.google.com/file/d/1hms42PdGqHVmWJ1pqyUhPXhwbFqzhsRf/view?usp=drive_link" target="_blank" rel="noreferrer">
+                MEU CV
+              </a>
           </button>
           <button className="lets-talk">
-          <span><a href="jsa.eric@hotmail.com">CONTATO</a></span>
+            <span><a href="jsa.eric@hotmail.com">CONTATO</a></span>
           </button>
         </div>
       </div>
+        <img src={Work} alt="work-in-progress" className="work-in-progress" id="work-in-progress"/>
         <img src={Imu} alt="eriques" className="hero-image" id="my-photo"/>
     </div>
     <div className="social-icons">
